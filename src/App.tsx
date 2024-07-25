c1814b3f85d136e533302248ecc1644a7882454f
jskimweb <jskimweb@gmail.com>
2024-07-25 18:17:41 +0900

    Add functionality to create new todo items

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,55 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useState, useRef } from "react";
+
+interface Todo {
+  id: number;
+  isDone: boolean;
+  content: string;
+  date: number;
+}
+
+const MOCK_DATA = [
+  {
+    id: 2,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 0,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(MOCK_DATA);
+  const id = useRef(3);
+
+  const onCreate = (content: string) => {
+    const newTodo = {
+      id: id.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,45 @@
 import "./Editor.css";
+import type { ChangeEvent, KeyboardEvent } from "react";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+interface EditorProps {
+  onCreate: (content: string) => void;
+}
+
+const Editor = ({ onCreate }: EditorProps) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onChangeContent = (e: ChangeEvent) => {
+    setContent((e.target as HTMLInputElement).value);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content.trim() === "") {
+      inputRef.current?.focus();
+    } else {
+      onCreate(content);
+    }
+
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={inputRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
